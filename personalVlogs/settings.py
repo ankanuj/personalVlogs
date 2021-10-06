7d75e75d434104de5fa205e1e00b16b380f8556e
ankanuj <s.singhanuj132@gmail.com>
2021-10-07 00:16:20 +0530

    added some file and configuration for heroku

--- personalVlogs/settings.py
+++ personalVlogs/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,3 +118,6 @@ STATICFILES_DIRS = [BASE_DIR / 'personalVlogs/static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
