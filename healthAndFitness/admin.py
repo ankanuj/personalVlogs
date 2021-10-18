22456913b72a5e3df1315b61d3687ff47a6c07eb
ankanuj <s.singhanuj132@gmail.com>
2021-10-18 17:37:24 +0530

    cretaed youtube page, created table for blog post

--- healthAndFitness/admin.py
+++ healthAndFitness/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import HealthAndFitnessBlog
+class HealthAndFitnessBlogAdmin(admin.ModelAdmin):
+  list_display = ('id','title','img','date','is_published')
+  list_display_links = ('id','title','is_published')
+  list_filter = ('title',)
+  search_fields = ('title',)
+  list_per_page = 25
 
-# Register your models here.
+admin.site.register(HealthAndFitnessBlog,HealthAndFitnessBlogAdmin)
--- healthAndFitness/models.py
+++ healthAndFitness/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class HealthAndFitnessBlog(models.Model):
+  title = models.CharField(max_length=250,blank=True)
+  img = models.ImageField(upload_to='blogsPhotos/%y/%m/%d/',blank=False,)
+  text = models.TextField(max_length=700)
+  date = models.DateTimeField(auto_now=False, auto_now_add=True)
+  is_published = models.BooleanField(default=True)
+
+  def __str__(self):
+        return self.title
